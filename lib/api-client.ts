b0a4364da1731b9314c49af7d3412f8f299f1201
public_yusuke <30387586+private-yusuke@users.noreply.github.com>
2021-03-23 17:59:02 +0900

    Create `api` function

---
+++ lib/api-client.ts
@@ -0,0 +1,19 @@
+import { API_HEADER_X_API_KEY } from "./constants";
+
+/**
+ * レシピ取得 API 等を利用する際に環境変数として指定されている API キーを利用して HTTP リクエストを行います。
+ * @param input `fetch` 関数で利用する引数
+ * @param init `fetch` 関数で利用する引数
+ * @returns API キーをヘッダに含めて fetch した際に生成される Promise オブジェクト
+ */
+export default function api(
+  input: RequestInfo,
+  init?: RequestInit
+): Promise<Response> {
+  let headers = {};
+  if (init) headers = init.headers;
+
+  headers[API_HEADER_X_API_KEY] = process.env.API_HEADER_X_API_KEY;
+
+  return fetch(input, { ...init, headers });
+}
--- lib/constants.ts
+++ lib/constants.ts
@@ -2,3 +2,4 @@ export const API_SERVER_ROOT = "https://internship-recipe-api.ckpd.co";
 export const API_ENDPOINT_RECIPES = `${API_SERVER_ROOT}/recipes`;
 export const API_ENDPOINT_SEARCH = `${API_SERVER_ROOT}/search`;
 export const API_ENDPOINT_IMAGE_URLS = `${API_SERVER_ROOT}/image_urls`;
+export const API_HEADER_X_API_KEY = "X-Api-Key";
