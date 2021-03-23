3bebfc9d10de59b4769cc41245f1693be5a6d617
azarashi2931 <46126763+azarashi2931@users.noreply.github.com>
2021-03-24 06:04:17 +0900

    add page previous/next button component

---
+++ components/Page.tsx
@@ -0,0 +1,33 @@
+import React, { FC } from "react";
+import { useRouter } from "next/router";
+import * as GetRecipes from "../@types/recipe-api/getRecipes";
+import * as SearchRecipes from "../@types/recipe-api/getSearch";
+
+type Props = {
+  response: GetRecipes.APIResponse | SearchRecipes.APISuccessResponse;
+};
+
+export const Page: FC<Props> = (props) => {
+  const router = useRouter();
+
+  const previous = () => {
+    if (props.response.links.prev) router.push(props.response.links.prev);
+  };
+
+  const next = () => {
+    if (props.response.links.next) router.push(props.response.links.next);
+  };
+
+  const isPreviousExists = props.response.links.prev != null;
+  const isNextExists = props.response.links.next != null;
+  return (
+    <div className="footer">
+      <button onClick={previous} disabled={!isPreviousExists}>
+        前へ
+      </button>
+      <button onClick={next} disabled={!isNextExists}>
+        次へ
+      </button>
+    </div>
+  );
+};
--- components/RecipeList.tsx
+++ components/RecipeList.tsx
@@ -1,29 +1,37 @@
 import React, { FC } from "react";
 import Link from "next/link";
-import { Recipe } from "../@types/recipe-api/recipe";
+import * as GetRecipes from "../@types/recipe-api/getRecipes";
+import * as SearchRecipes from "../@types/recipe-api/getSearch";
+import { Page } from "./Page";
 
-type Props = { recipes: Recipe[] };
+type Props = {
+  response: GetRecipes.APIResponse | SearchRecipes.APISuccessResponse;
+};
 
 export const RecipeList: FC<Props> = (props) => {
   return (
-    <ol className="recipeList">
-      {props.recipes.map((recipe, i) => (
-        <li key={i}>
-          <article>
-            <Link href={"/recipes/" + recipe.id.toString()}>
-              <div className="recipeListItem">
-                {recipe.image_url && (
-                  <img src={recipe.image_url} alt="レシピ画像" />
-                )}
-                <div>
-                  <h2>{recipe.title}</h2>
-                  <p>{recipe.description}</p>
+    <div>
+      <ol className="recipeList">
+        {props.response.recipes.map((recipe, i) => (
+          <li key={i}>
+            <article>
+              <Link href={"/recipes/" + recipe.id.toString()}>
+                <div className="recipeListItem">
+                  {recipe.image_url && (
+                    <img src={recipe.image_url} alt="レシピ画像" />
+                  )}
+                  <div>
+                    <h2>{recipe.title}</h2>
+                    <p>{recipe.description}</p>
+                  </div>
                 </div>
-              </div>
-            </Link>
-          </article>
-        </li>
-      ))}
-    </ol>
+              </Link>
+            </article>
+          </li>
+        ))}
+      </ol>
+
+      <Page response={props.response} />
+    </div>
   );
 };
