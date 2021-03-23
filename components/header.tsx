dfb6868bd9f94438995a4b3c674ae8fedcc642c2
public_yusuke <30387586+private-yusuke@users.noreply.github.com>
2021-03-24 00:44:06 +0900

    Add search button

--- components/header.tsx
+++ components/header.tsx
@@ -5,12 +5,19 @@ type Props = {
 };
 
 const Header: FC<Props> = (props) => {
-  const onKeyPress = (e) => {
-    // 検索窓に何かが入力されていて、かつエンターが押下されたとき検索を開始
-    if (e.which == 13) {
-      const keyword = e.target.value;
-      if (keyword) location.href = `/search?keyword=${keyword}`;
-    }
+  const SEARCH_BAR_INPUT_ID = "search-bar-input";
+
+  const onSearchSubmitted = (e) => {
+    // エンターが押下されたとき検索を開始
+    if (e.which == 13) onSearchTriggered();
+  };
+
+  const onSearchTriggered = () => {
+    const elem = document.getElementById(SEARCH_BAR_INPUT_ID);
+    const keyword = elem.value;
+
+    // 検索窓に何かが入力されていた場合、検索を開始
+    if (keyword) location.href = `/search?keyword=${keyword}`;
   };
 
   return (
@@ -21,15 +28,19 @@ const Header: FC<Props> = (props) => {
         </a>
       </div>
 
-      <div className="p-4 bg-gray-100">
+      <div className="p-4 bg-gray-100 w-full flex justify-content">
         <input
           type="search"
           name="search"
           defaultValue={props.searchQuery}
           placeholder="検索"
-          className="border-2 border-gray-300 bg-white h-10 w-full px-5  object-center rounded-lg text-sm focus:outline-none"
-          onKeyPress={onKeyPress}
+          className="border-2 border-gray-300 bg-white h-10 pl-5 w-full object-center rounded-lg text-sm focus:outline-none"
+          id={SEARCH_BAR_INPUT_ID}
+          onKeyPress={onSearchSubmitted}
         />
+        <button className="px-2 text-3xl" onClick={onSearchTriggered}>
+          🔎
+        </button>
       </div>
     </header>
   );
