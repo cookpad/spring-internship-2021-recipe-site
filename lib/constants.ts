30adfb62adad56ff0a2e8044b3c02554a20a791d
public_yusuke <30387586+private-yusuke@users.noreply.github.com>
2021-03-23 17:07:06 +0900

    Create constants.ts

---
+++ lib/constants.ts
@@ -0,0 +1,4 @@
+export const API_SERVER_ROOT = "https://internship-recipe-api.ckpd.co";
+export const API_ENDPOINT_RECIPES = `${API_SERVER_ROOT}/recipes`;
+export const API_ENDPOINT_SEARCH = `${API_SERVER_ROOT}/search`;
+export const API_ENDPOINT_IMAGE_URLS = `${API_SERVER_ROOT}/image_urls`;
